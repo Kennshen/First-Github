4aae093c0763e84b49a19f6392283a5a1a4444cc
Chris Choi <ckchoi1814@gmail.com>
2022-10-05 12:39:44 -0400

    Adding github practice code

---
+++ Github_practice_code.py
@@ -0,0 +1,19 @@
+def length_conversion(value, units):
+    if units == "mm":
+        new_value = float(value)*25.4
+        return new_value
+    elif units == "in":
+        new_value = float(value)/25.4
+        return new_value
+    else:
+        print("Please use mm or in")
+        return False
+
+Success = False
+while Success == False:
+    ask_value = input("What is the value you want to convert?: ")
+    ask_unit = input("What is the value you want to convert?: ")
+    Success = length_conversion(ask_value, ask_unit)
+
+print(f"The converted value is {Success} {ask_unit}")
+
